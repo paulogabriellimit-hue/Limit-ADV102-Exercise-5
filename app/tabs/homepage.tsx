ca091526ec70184453eae1e5bea444c97854336f
paulogabriellimit-hue <paulogabriel.limit@hcdc.edu.ph>
2026-02-22 13:05:36 +0800

    Add Homepage component with welcome message

---
+++ app/tabs/homepage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Homepage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Homepage 🎉</h1>
+      <p>You are successfully logged in!</p>
+    </div>
+  );
+};
+
+export default Homepage;
